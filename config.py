ab1c75c8f3be0d83689dc9860d756fae82c9b303
ZHR-Harris <52810398+ZHR-Harris@users.noreply.github.com>
2020-12-20 21:41:43 +0800

    database creation;404 and login inheritance done

---
+++ config.py
@@ -0,0 +1,14 @@
+# dialect+driver://username:password@host:port/database
+CSRF_ENABLED = True
+SECRET_KEY = '\xca\x0c\x86\x04\x98@\x02b\x1b7\x8c\x88]\x1b\xd7"+\xe6px@\xc3#\\'
+DIALECT = 'mysql'
+DRIVER = 'pymysql'
+USERNAME = 'root'
+PASSWORD = '123456'
+HOST = '127.0.0.1'
+PORT = '3306'
+DATABASE = 'vmarto'
+
+DEBUG = True
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_DATABASE_URI = "{}+{}://{}:{}@{}:{}/{}?charset=utf8".format(DIALECT,DRIVER,USERNAME,PASSWORD,HOST,PORT,DATABASE)
